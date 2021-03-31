1dbeb797d29e385d4318a87e1cd10512847d8a5d
PROBOY X <68194873+PROBOYX@users.noreply.github.com>
2021-03-31 18:26:59 +0530

    Done

--- upp.py
+++ upp.py
@@ -6,8 +6,8 @@ from contextlib import suppress
 import os
 import sys
 import asyncio
-from ULTRA import CMD_HELP
-from ULTRA.utils import admin_cmd
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd
 
 # -- Constants -- #
 IS_SELECTED_DIFFERENT_BRANCH = (
@@ -16,8 +16,8 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/PROBOYX/HEROKU"
-BOT_IS_UP_TO_DATE = "âUltra xis up-to-date!â"
+OFFICIAL_UPSTREAM_REPO = "https://github.com/ULTRA-OP/HEROKU"
+BOT_IS_UP_TO_DATE = "Ultra xis up-to-date!"
 NEW_BOT_UP_DATE_FOUND = (
     "**ULTRA X  Update Found For** {branch_name}\n"
     "\n\n{changelog}\n"
@@ -73,7 +73,7 @@ async def updater(message):
     )
 
     if not changelog:
-        await message.edit("**ï¹ï¹Updating Ultra X* \n**Version** : `1.0.0` \n**Telethon** : `1.19.5` \n**Status** : `Â«Pulling UpdatesÂ»` \nã**Thank You For Using Ultra X !**ã")
+        await message.edit("**Updating Ultra X* \n**Version** : `1.0.0` \n**Telethon** : `1.19.5` \n**Status** : `Â«Pulling UpdatesÂ»` \n**Thank You For Using Ultra X !**")
         await asyncio.sleep(5)
  
     message_one = NEW_BOT_UP_DATE_FOUND.format(
@@ -141,7 +141,7 @@ def generate_change_log(git_repo, diff_marker):
 
 async def deploy_start(bot, message, refspec, remote):
     await message.edit(RESTARTING_APP)
-    await message.edit("**ULTRA X** \n**Version** : `2.0` \n**Telethon** : `1.15.0` \n**Branch** : main \n**Status** : `âUpdating & Restarting` \n__You Can Do__ `.alive` __To Check If I am Alive !__â")
+    await message.edit("**ULTRA X** \n**Version** : `2.0` \n**Telethon** : `1.15.0` \n**Branch** : main \n**Status** : `Updating & Restarting` \n__You Can Do__ `.alive` __To Check If I am Alive !")
     remote.push(refspec=refspec)
     await bot.disconnect()
     os.execl(sys.executable, sys.executable, *sys.argv)
